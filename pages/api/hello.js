98f3a7c93d8c6184eb40a04c44f0f22184796ef0
jun-5 <rkd7997@gmail.com>
2022-12-19 08:28:16 +0900

    test view

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const testView = {
+    type: "home",
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "A simple stack of blocks for the simple sample Block Kit Home tab.",
+        },
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Action A",
+              emoji: true,
+            },
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Action B",
+              emoji: true,
+            },
+          },
+        ],
+      },
+    ],
+  };
+  res.status(200).json(testView);
 }
